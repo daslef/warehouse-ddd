168bd06c210715dc62943820f0096747d6e25202
Lex Trofimov <daslef93@gmail.com>
2023-10-04 22:09:27 +0300

    implemented allocate service

--- tests/test_allocate.py
+++ tests/test_allocate.py
@@ -1,8 +1,11 @@
 from datetime import date, timedelta
 import pytest
 
+from model import Batch, OrderLine
+from allocate import allocate
+from exceptions import OutOfStock
 
-@pytest.mark.skip(reason="allocate service is not implemented yet")
+
 def test_prefers_warehouse_batches_to_shipments():
     """
     arrange: создать батч, доступный на складе, и батч находящийся в пути; создать товарную позицию;
@@ -10,35 +13,61 @@ def test_prefers_warehouse_batches_to_shipments():
     assert: убедиться, что размещение произошло в складском батче (уменьшилось доступное количество),
             а батч в пути не изменился;
     """
-    pytest.fail("todo")
+
+    warehouse_batch = Batch("warehouse-001", "PRETTY_CHAIR", 20)
+    shipping_batch = Batch("shipping-001", "PRETTY_CHAIR", 20, eta=date.today())
+    line = OrderLine("chairs-001", "PRETTY_CHAIR", 10)
+
+    allocate(line, [shipping_batch, warehouse_batch])
+
+    assert warehouse_batch.available_quantity == 10
+    assert shipping_batch.available_quantity == 20
 
 
-@pytest.mark.skip(reason="allocate service is not implemented yet")
 def test_prefers_earlier_batches():
     """
     arrange: создать батчи, доступные сегодня, завтра и через неделю;
     act: запросить сервис allocate
     assert: убедиться, что размещение произошло в сегодняшнем батче;
     """
-    pytest.fail("todo")
+
+    today_batch = Batch("shipping-001", "PRETTY_CHAIR", 20, eta=date.today())
+    tomorrow_batch = Batch(
+        "shipping-002", "PRETTY_CHAIR", 20, eta=date.today() + timedelta(days=1)
+    )
+    week_batch = Batch(
+        "shipping-003", "PRETTY_CHAIR", 20, eta=date.today() + timedelta(days=7)
+    )
+    line = OrderLine("chairs-001", "PRETTY_CHAIR", 10)
+
+    allocate(line, [tomorrow_batch, week_batch, today_batch])
+
+    assert today_batch.available_quantity == 10
 
 
-@pytest.mark.skip(reason="allocate service is not implemented yet")
 def test_returns_allocated_batch_ref():
     """
-    arrange: создать два батча и товарную позицию;
+    arrange: создать батч и товарную позицию;
     act: запросить сервис allocate
     assert: убедиться, что сервис allocate вернул значение,
-            эквивалентное референсу выбранного батча
+            эквивалентное референсу батча
     """
-    pytest.fail("todo")
+
+    batch = Batch("shipping-001", "PRETTY_CHAIR", 20, eta=date.today())
+    line = OrderLine("chairs-001", "PRETTY_CHAIR", 10)
+
+    assert allocate(line, [batch]) == batch.reference
 
 
-@pytest.mark.skip(reason="allocate service is not implemented yet")
 def test_raises_out_of_stock_exception_if_cannot_allocate():
     """
-    arrange: создать батч и разместить в нем заказ на весь объем;
-    act: запросить дополнительное размещение в том же батче;
+    arrange: создать батч и товарную позицию;
+    act: разместить в батче заказ на больший объем;
     assert: убедиться, что сервис allocate выбросил ошибку OutOfStock
     """
-    pytest.fail("todo")
+
+    batch = Batch("shipping-001", "PRETTY_CHAIR", 20, eta=date.today())
+    line = OrderLine("chairs-001", "PRETTY_CHAIR", 30)
+
+    with pytest.raises(OutOfStock):
+        allocate(line, [batch])
