2cdd1d4cb22b9c8177aa5b1a06ceb853de58de53
Lex Trofimov <daslef93@gmail.com>
2023-12-19 22:17:11 +0300

    batches template & view; dockerize;

--- flask_app.py
+++ flask_app.py
@@ -23,15 +23,32 @@ except Exception:
 app = Flask(__name__)
 
 
+@app.route("/admin/batches", methods=["GET", "POST"])
+def admin_batches_view():
+    session = get_session()
+    repo = repository.SqlAlchemyRepository(session)
+
+    if request.method == "POST":
+        reference = request.form.get("reference")
+        sku = request.form.get("sku")
+        qty = request.form.get("qty")
+        eta = request.form.get("eta")
+
+        repo.add(model.Batch(reference, sku, qty, eta))
+        session.commit()
+
+    batches = repo.list()
+    return render_template("batches.html", batches=batches)
+
+
 @app.route("/admin")
 def admin_view():
     session = get_session()
     repo = repository.SqlAlchemyRepository(session)
     batches = repo.list()
     allocations = [b.allocations for b in batches]
-    print(batches, allocations)
 
-    return render_template("admin.html", orderlines=allocations)
+    return render_template("admin.html", orderlines=allocations, batches=batches)
 
 
 @app.route("/api/allocate", methods=["POST"])
